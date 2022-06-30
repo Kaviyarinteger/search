b9a483c25bafa8146e735cfbbf0636a14efb08fc
Kaviyarinteger <105354318+Kaviyarinteger@users.noreply.github.com>
2022-06-30 12:12:38 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -60,8 +60,8 @@ d31=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSOgtXR4WBKKi3J
 d32=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSOgtXR4WBKKi3JZlsYsnWBj5N4TwySNeTlXMLT07cKpqWV_7J1iUBfydGZqloLRQ/pub?gid=761036551&single=true&output=csv")
 d33=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSOgtXR4WBKKi3JZlsYsnWBj5N4TwySNeTlXMLT07cKpqWV_7J1iUBfydGZqloLRQ/pub?gid=1838378944&single=true&output=csv")
 d34=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSOgtXR4WBKKi3JZlsYsnWBj5N4TwySNeTlXMLT07cKpqWV_7J1iUBfydGZqloLRQ/pub?gid=1125510532&single=true&output=csv")
-d35=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSOgtXR4WBKKi3JZlsYsnWBj5N4TwySNeTlXMLT07cKpqWV_7J1iUBfydGZqloLRQ/pub?gid=1333070184&single=true&output=csv")
-d36=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTvjmT79X4qi5lARzUZoCZLwS7uShtJVLQ6pq2Zsf51fQwUmaNKYAiRT0HAQ6IIiw/pub?gid=1094974181&single=true&output=csv")
+#d35=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSOgtXR4WBKKi3JZlsYsnWBj5N4TwySNeTlXMLT07cKpqWV_7J1iUBfydGZqloLRQ/pub?gid=1333070184&single=true&output=csv")
+#d36=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTvjmT79X4qi5lARzUZoCZLwS7uShtJVLQ6pq2Zsf51fQwUmaNKYAiRT0HAQ6IIiw/pub?gid=1094974181&single=true&output=csv")
 
 result = d2.append([d2, d4 ,d5 ,d6 ,d7 ,d8 ,d9 , d11, d12 ,d13 ,d14 ,d15 ,d16 ,d17 ,d18 ,d19 ,d20 ,d21 ,d22 ,d23 ,d24 ,d25 ,d26 ,d27 ,d28 ,d29 ,d30 ,d31 ,d32 ,d33 ,d34 ,d35 ,d36])
 
